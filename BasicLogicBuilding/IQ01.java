f4ab8a8b378af0f8ce2ebd56fd304bfda4e09f38
Deepak <darkdeepak001@gmail.com>
2026-04-04 22:12:47 +0530

    Implement user authentication with OTP verification

---
+++ BasicLogicBuilding/IQ01.java
@@ -0,0 +1,40 @@
+import java.lang.System;
+import java.util.Scanner;
+class IQ01
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String user_name = "Deepak";
+        String password = "2911";
+        int otp = 249934;
+        System.out.print("Enter Your user name : ");
+        String u_name = scan.nextLine();
+        if(user_name.equals(u_name))
+        {
+            System.out.print("Enter your password : ");
+            String p = scan.nextLine();
+            if(password.equals(p))
+            {
+                System.out.print("Enter  otp : ");
+                int o = scan.nextInt();
+                if(otp==o)
+                {
+                    System.out.print("Login successful");
+                }
+                else
+                {
+                    System.out.print("Incorrect otp entered");
+                }
+            }
+            else
+            {
+            System.out.print("Incorrect Password entered");
+            }
+        }
+        else 
+        {
+            System.out.print("Incorrect User name entered");
+        }
+    }
+}
