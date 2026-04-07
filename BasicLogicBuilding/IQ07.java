ab2b6af93b54c0cdadce62fb6b06dfd41b0b0359
Deepak <darkdeepak001@gmail.com>
2026-04-07 20:20:17 +0530

    Create IQ07.java

---
+++ BasicLogicBuilding/IQ07.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+import java.util.Scanner;
+class IQ07
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number of Students who came to return their books : ");
+        int student_count = scan.nextInt();
+        
+    }
+}
