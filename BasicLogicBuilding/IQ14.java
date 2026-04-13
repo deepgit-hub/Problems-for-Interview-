a1a0fcb49ea3cef0ba8d280d1ef8409210e5fcc8
Deepak <darkdeepak001@gmail.com>
2026-04-13 20:17:20 +0530

    Update and rename sss to IQ14.java

---
+++ BasicLogicBuilding/IQ14.java
@@ -0,0 +1,50 @@
+import java.lang.System;
+import java.util.Scanner;
+class IQ14
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Number of Customers : ");
+        int customer_count = scan.nextInt();
+        int[] purchase = new int[customer_count];
+        int discount = 0; 
+        int highest_purchase ;
+        int gift = 0;
+        for(int i=0;i<customer_count;i++)
+        {
+           System.out.println("Customer : "+(i+1));
+           System.out.print("Enter the amount you purchased : ");
+           purchase[i]= scan.nextInt();
+        }
+        for(int j=0;j<customer_count;j++)
+        {
+           if(purchase[j]>2000 && purchase[j]<5000)
+           {
+             discount += purchase[j]*10/100;
+           }
+           else if(purchase[j]>=5000 && purchase[j]<10000)
+           {
+             discount += purchase[j]*15/100;
+           }
+           else if(purchase[j]>=10000)
+           {
+             discount += purchase[j]*25/100;
+             gift++;
+           }
+
+        }
+        highest_purchase = purchase[0];
+        for(int k=1;k<customer_count;k++)
+        {
+            if(highest_purchase<purchase[k])
+            {
+                highest_purchase= purchase[k];
+            }
+        }
+        System.out.println("The total discount given : "+discount);
+        System.out.println("The highest purchased is "+highest_purchase);
+        System.out.println("Gift given : "+gift);
+    
+    }
+}
