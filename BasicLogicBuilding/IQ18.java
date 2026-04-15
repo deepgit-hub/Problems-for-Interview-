708246469b46c8cb962a85f8b29e8a5d131eba35
Deepak <darkdeepak001@gmail.com>
2026-04-15 21:36:54 +0530

    Create IQ18.java

---
+++ BasicLogicBuilding/IQ18.java
@@ -0,0 +1,44 @@
+import java.lang.System;
+import java.util.Scanner;
+class IQ18
+{
+    public static void main(String args[])
+    {
+        void biggestnum(int a,int b,int c)
+        {
+            if(a>=b && a>=c)
+            {
+                System.out.print("The Greatest Number amoung the given number is "+a);
+            }
+            else if(b>=a && b>=c)
+            {
+                System.out.print("The Greatest Number amount the given number is "+b);
+            }
+            else 
+            {
+                System.out.print("The Greatest Number amount the given number is "+c);
+            }
+        }
+        void smallestnum(int a,int b,int c)
+        {
+            if(a<=b && a<=c)
+            {
+                System.out.print("The Smallest Number amoung the given number is "+a);
+            }
+            else if(b<=a && b<=c)
+            {
+                System.out.print("The Smallest Number amount the given number is "+b);
+            }
+            else 
+            {
+                System.out.print("The Smallest Number amount the given number is "+c);
+            }
+        }
+        public static void main(String args[])
+        {
+            Practice p = new Practice();
+            p.biggestnum(13,56,76);
+            p.smallestnum(8,23,43);
+        }
+    }
+}
