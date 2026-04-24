fd23ab662866b0936009221a5e9b07d9a965179b
Deepak <darkdeepak001@gmail.com>
2026-04-24 13:41:35 +0530

    Change instance creation from IQ01 to IQ22

---
+++ BasicLogicBuilding/IQ22.java
@@ -0,0 +1,29 @@
+import java.lang.System;
+import java.util.Scanner;
+class IQ22
+{
+    String name;
+    int roll_no;
+    int reg_no;
+    String department;
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        IQ22 a = new IQ22();
+        System.out.print("Enter Student Name : ");
+        a.name=scan.nextLine();
+        System.out.print("Enter Roll number : ");
+        a.roll_no = scan.nextInt();
+        System.out.print("Enter Reg Number : ");
+        a.reg_no=scan.nextInt();
+        scan.nextLine();
+        System.out.print("Enter Department : ");
+        a.department=scan.nextLine();
+        System.out.println("Student Details");
+        System.out.println("Name        : "+a.name);
+        System.out.println("Roll no     : "+a.roll_no);
+        System.out.println("Reg no      : "+a.reg_no);
+        System.out.println("Department  : "+a.department);
+
+    }
+}
