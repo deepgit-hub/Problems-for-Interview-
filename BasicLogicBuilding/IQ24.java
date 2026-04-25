359d3236f202fb3f5edeb720c5c6231b6ace3e18
Deepak <darkdeepak001@gmail.com>
2026-04-25 13:28:58 +0530

    Create IQ24.java

---
+++ BasicLogicBuilding/IQ24.java
@@ -0,0 +1,13 @@
+📌 Interview Question
+
+Write a Java program using classes and objects to store details of multiple phones and find the most expensive one.
+
+The program should:
+
+Store:
+Brand name
+Price
+Storage
+Take input for 3 phones
+Display all phone details
+Compare and print the phone with highest price
