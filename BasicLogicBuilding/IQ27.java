6ec94da9334725e832d9c6c4027d0511000a81a7
Deepak <darkdeepak001@gmail.com>
2026-04-27 21:58:45 +0530

    Update IQ27.java

--- BasicLogicBuilding/IQ27.java
+++ BasicLogicBuilding/IQ27.java
@@ -1,3 +1,23 @@
+/* 📌 Interview Question
+
+Design a program to store and display student details using a constructor and a method.
+
+The system should:
+
+Create a class with instance variables:
+    - register number (int)
+    - department (String)
+
+Use a constructor to:
+    - Initialize the register number and department of the student
+
+Create a method to:
+    - Display the student's register number and department
+
+Finally:
+    - Create an object with sample values
+    - Call the display method to show the details
+*
 import java.lang.System;
 import java.util.Scanner;
 class IQ27
