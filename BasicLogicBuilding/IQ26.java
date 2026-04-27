c4774093c5556f3e4bcb0734cf91b889a3e25529
Deepak <darkdeepak001@gmail.com>
2026-04-27 21:51:46 +0530

    Update IQ26.java

--- BasicLogicBuilding/IQ26.java
+++ BasicLogicBuilding/IQ26.java
@@ -1,3 +1,21 @@
+/* 📌 Interview Question
+
+Design a program to store and display basic details of a person using a constructor.
+
+The system should:
+
+Create a class with instance variables:
+    - name (String)
+    - age (int)
+
+Use a constructor to:
+    - Initialize the name and age of the person
+
+When an object is created:
+    - Automatically display the person's name and age using the constructor
+
+Finally, demonstrate the program by creating an object with sample values.
+
 import java.lang.System;
 import java.util.Scanner;
 class IQ26
